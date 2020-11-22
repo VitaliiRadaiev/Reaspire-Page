d8699526fb01ac3e5ca8eef7199440d49820c72a
VitaliiRadaiev <hf.chelenger@gmail.com>
2020-11-22 12:10:12 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,325 @@
+var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+
+//SlideToggle
+let _slideUp = (target, duration = 500) => {
+	target.style.transitionProperty = 'height, margin, padding';
+	target.style.transitionDuration = duration + 'ms';
+	target.style.height = target.offsetHeight + 'px';
+	target.offsetHeight;
+	target.style.overflow = 'hidden';
+	target.style.height = 0;
+	target.style.paddingTop = 0;
+	target.style.paddingBottom = 0;
+	target.style.marginTop = 0;
+	target.style.marginBottom = 0;
+	window.setTimeout(() => {
+		target.style.display = 'none';
+		target.style.removeProperty('height');
+		target.style.removeProperty('padding-top');
+		target.style.removeProperty('padding-bottom');
+		target.style.removeProperty('margin-top');
+		target.style.removeProperty('margin-bottom');
+		target.style.removeProperty('overflow');
+		target.style.removeProperty('transition-duration');
+		target.style.removeProperty('transition-property');
+		target.classList.remove('_slide');
+	}, duration);
+}
+let _slideDown = (target, duration = 500) => {
+	target.style.removeProperty('display');
+	let display = window.getComputedStyle(target).display;
+	if (display === 'none')
+		display = 'block';
+
+	target.style.display = display;
+	let height = target.offsetHeight;
+	target.style.overflow = 'hidden';
+	target.style.height = 0;
+	target.style.paddingTop = 0;
+	target.style.paddingBottom = 0;
+	target.style.marginTop = 0;
+	target.style.marginBottom = 0;
+	target.offsetHeight;
+	target.style.transitionProperty = "height, margin, padding";
+	target.style.transitionDuration = duration + 'ms';
+	target.style.height = height + 'px';
+	target.style.removeProperty('padding-top');
+	target.style.removeProperty('padding-bottom');
+	target.style.removeProperty('margin-top');
+	target.style.removeProperty('margin-bottom');
+	window.setTimeout(() => {
+		target.style.removeProperty('height');
+		target.style.removeProperty('overflow');
+		target.style.removeProperty('transition-duration');
+		target.style.removeProperty('transition-property');
+		target.classList.remove('_slide');
+	}, duration);
+}
+let _slideToggle = (target, duration = 500) => {
+	if (!target.classList.contains('_slide')) {
+		target.classList.add('_slide');
+		if (window.getComputedStyle(target).display === 'none') {
+			return _slideDown(target, duration);
+		} else {
+			return _slideUp(target, duration);
+		}
+	}
+}
+//========================================
+
+
+
+
+// === Конвертация svg картинки в svg код ==================================================================
+$('img.img-svg').each(function(){
+  var $img = $(this);
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+  $.get(imgURL, function(data) {
+    var $svg = $(data).find('svg');
+    if(typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+    $svg = $svg.removeAttr('xmlns:a');
+    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    }
+    $img.replaceWith($svg);
+  }, 'xml');
+});
+// === // Конвертация svg картинки в svg код ==================================================================
+
+
+
+//Spollers
+
+	let spollers = document.querySelectorAll("._spoller");
+	if (spollers.length > 0) {
+		for (let index = 0; index < spollers.length; index++) {
+			const spoller = spollers[index];
+			spoller.addEventListener("click", function (e) {
+				e.preventDefault();
+				if (spoller.classList.contains('_spoller-992') && window.innerWidth > 992) {
+					return false;
+				}
+				if (spoller.classList.contains('_spoller-768') && window.innerWidth > 768) {
+					return false;
+				}
+				if (spoller.closest('._spollers').classList.contains('_one')) {
+					let curent_spollers = spoller.closest('._spollers').querySelectorAll('._spoller');
+					for (let i = 0; i < curent_spollers.length; i++) {
+						let el = curent_spollers[i];
+						if (el != spoller) {
+							el.classList.remove('_active');
+							_slideUp(el.nextElementSibling);
+						}
+					}
+				}
+				spoller.classList.toggle('_active');
+				_slideToggle(spoller.nextElementSibling);
+			});
+		}
+	}
+
+// === // Spollers ==================================================================
+
+
+// === Tabs ==================================================================
+{
+	let tabs = document.querySelector('.tabs');
+	if(tabs) {
+		tabs.querySelectorAll('.tabs__triggers').forEach((item) => {
+			item.addEventListener('click', function(e) {
+				e.preventDefault();
+				const id = e.target.getAttribute('href').replace('#','');
+
+				tabs.querySelectorAll('.tabs__triggers').forEach((child) => {
+					child.classList.remove('active');
+				});
+
+				tabs.querySelectorAll('.tabs__content').forEach((child) => {
+					child.classList.remove('active');
+				});
+
+				item.classList.add('active');
+				document.getElementById(id).classList.add('active');
+			});
+		});
+	}
+}
+// === // Tabs ==================================================================;
+
+
+$(document).ready(function() {
+	document.body.classList.add('is-load');
+
+	// === Burger Handler =====================================================================
+	function burgerBtnAnimation(e) {
+		$('.burger span:nth-child(1)').toggleClass('first');
+		$('.burger span:nth-child(2)').toggleClass('second');
+		$('.burger span:nth-child(3)').toggleClass('third');
+		$('.burger span:nth-child(4)').toggleClass('fourth');
+		let classNameElem = document.querySelector('.burger').dataset.activel;
+		document.querySelector(`.${classNameElem}`).classList.toggle('open');
+		_slideToggle(document.querySelector(`.${classNameElem}`));
+	}
+	$('.burger').click((e) => burgerBtnAnimation(e));
+// === Burger Handler =====================================================================	;
+
+
+// === Проверка, поддержка браузером формата webp ==================================================================
+
+	function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+	callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	}
+
+	testWebP(function (support) {
+
+	if (support == true) {
+	document.querySelector('body').classList.add('webp');
+	}else{
+	document.querySelector('body').classList.add('no-webp');
+	}
+	});
+
+// === // Проверка, поддержка браузером формата webp ==================================================================
+
+function cardVideoHandler() {
+	function togglePlayPause(video,btn) {
+		if(video.paused) {
+			video.play();
+			btn.firstElementChild.className = 'icon-pause_circle_filled';
+			btn.firstElementChild.style.marginLeft = '0px';
+
+		} else {
+			video.pause();
+			btn.firstElementChild.className = 'icon-play_circle_filled';
+		}
+	}
+
+	let videoBlock = document.querySelectorAll('.video-block');
+	if(videoBlock.length) {
+		let timerId;
+		videoBlock.forEach((item) => {
+
+			//let videoWrap = card.querySelector('.card-video__video-wrap');
+			let video = item.querySelector('.video-block__video');
+			let btn = item.querySelector('.video-block__play-pause');
+			//let time = item.querySelector('.card-video__duration-time');
+			//let btnLink = item.querySelector('.card-video__btn');
+
+			if(video) {
+				btn.addEventListener('click', (e) => {
+					e.preventDefault();
+					togglePlayPause(video,btn);
+				});
+				video.addEventListener('ended', () => {
+					video.pause();
+					btn.firstElementChild.className = 'icon-play_circle_filled';
+				});
+				video.addEventListener('mousemove', (e) => { 
+					if(!video.paused) {
+						btn.style.opacity = '1';
+						
+							clearTimeout(timerId);
+							timerId = setTimeout(() => {
+								btn.style.opacity = '0';
+							}, 2000);
+
+					} else {
+						btn.style.opacity = '1';
+					}
+
+				});
+
+			}
+		})
+	}
+
+}
+
+cardVideoHandler();;
+{
+	let slider = document.querySelector('.slider .swiper-container');
+	if(slider) {
+		let dataSlider = new Swiper(slider, {
+			/*
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+			//observer: true,
+			//observeParents: true,
+			slidesPerView: 3,
+			spaceBetween: 25,
+			//autoHeight: true,
+			speed: 800,
+			//touchRatio: 0,
+			//simulateTouch: false,
+			loop: true,
+			//preloadImages: false,
+			//lazy: true,
+			// Dotts
+			//pagination: {
+			//	el: '.slider-quality__pagging',
+			//	clickable: true,
+			//},
+			// Arrows
+			navigation: {
+				nextEl: '.slider__btn-next',
+				prevEl: '.slider__btn-prev',
+			},
+			
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 15,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 25,
+				},
+
+			},
+			
+
+			// And if we need scrollbar
+			//scrollbar: {
+			//	el: '.swiper-scrollbar',
+			//},
+		});
+	}
+};
+{
+  let gallerySlider = document.querySelector('.gallery-slider');
+  if(gallerySlider) {
+    var galleryThumbs = new Swiper(gallerySlider.querySelector('.gallery-slider__thumbs'), {
+      spaceBetween: 10,
+      slidesPerView: 'auto',
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper(gallerySlider.querySelector('.gallery-slider__top'), {
+      spaceBetween: 0,
+      thumbs: {
+        swiper: galleryThumbs
+      }
+    });
+  }
+}
+;
+
+});
